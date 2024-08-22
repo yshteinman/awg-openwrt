c9e60562043ae8c965888b2d55cc310e9e5cc29d
lolo6oT <lolo6oT@gmail.com>
2024-08-22 12:31:16 +0300

    Update amneziawg.js

--- luci-proto-amneziawg/htdocs/luci-static/resources/protocol/amneziawg.js
+++ luci-proto-amneziawg/htdocs/luci-static/resources/protocol/amneziawg.js
@@ -1254,10 +1254,12 @@ return network.registerProtocol("amneziawg", {
 				headNode = mapNode.parentNode.querySelector("h4"),
 				configGenerator = this.createPeerConfig.bind(this, section_id),
 				parent = this.map;
+				    eips = this.section.formvalue(section_id, 'allowed_ips');
 
 			return Promise.all([
 				network.getWANNetworks(),
 				network.getWAN6Networks(),
+				network.getNetwork('lan'),
 				L.resolveDefault(uci.load("ddns")),
 				L.resolveDefault(uci.load("system")),
 				parent.save(null, true),
@@ -1291,11 +1293,21 @@ return network.registerProtocol("amneziawg", {
 					);
 
 				var ips = ["0.0.0.0/0", "::/0"];
+				
+				var dns = [];
+
+				var lan = data[2];
+				if (lan) {
+					var lanIp = lan.getIPAddr();
+					if (lanIp) {
+						dns.unshift(lanIp)
+					}
+				}
 
 				var qrm, qrs, qro;
 
 				qrm = new form.JSONMap(
-					{ config: { endpoint: hostnames[0], allowed_ips: ips } },
+					{ config: { endpoint: hostnames[0], allowed_ips: ips, addresses: eips, dns_servers: dns } },
 					null,
 					_(
 						"The generated configuration can be imported into a AmneziaWG client application to set up a connection towards this device."
@@ -1309,7 +1321,9 @@ return network.registerProtocol("amneziawg", {
 					var code = this.map.findElement(".qr-code"),
 						conf = this.map.findElement(".client-config"),
 						endpoint = this.section.getUIElement(section_id, "endpoint"),
-						ips = this.section.getUIElement(section_id, "allowed_ips");
+						ips = this.section.getUIElement(section_id, "allowed_ips"),
+						eips = this.section.getUIElement(section_id, 'addresses'),
+						dns = this.section.getUIElement(section_id, 'dns_servers');
 
 					if (this.isValid(section_id)) {
 						conf.firstChild.data = configGenerator(
@@ -1351,9 +1365,20 @@ return network.registerProtocol("amneziawg", {
 				});
 				qro.onchange = handleConfigChange;
 
+				qro = qrs.option(form.DynamicList, "dns_servers", _("DNS Servers"), _("DNS servers for the remote clients using this tunnel to your openwrt device. Some AmneziaWG clients require this to be set."));
+				qro.datatype = "ipaddr";
+				qro.default = dns;
+				qro.onchange = handleConfigChange;
+
+				qro = qrs.option(form.DynamicList, "addresses", _("Addresses"), _("IP addresses for the peer to use inside the tunnel. Some clients require this setting."));
+				qro.datatype = 'ipaddr';
+				qro.default = eips;
+				eips.forEach(function(eip) { qro.value(eip) });
+				qro.onchange = handleConfigChange;
+
 				qro = qrs.option(form.DummyValue, "output");
 				qro.renderWidget = function () {
-					var peer_config = configGenerator(hostnames[0], ips);
+					var peer_config = configGenerator(hostnames[0], ips, eips, dns);
 
 					var node = E(
 						"div",
